0f33643260e97ec283ed692c56ecb3bcdd7d5074
MikhailPakaliuk <mr.dyurilen@mail.ru>
2019-10-08 15:48:20 +0300

    update index.js
    feat: add function matching brackets

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
+    
+    function couple(value,array){
+        let result;
+        for(let i=0;i<array.length;i++){
+            if(array[i][0]==value){
+                result = array[i][1];
+            }
+        }
+    return result;
+    }
+    
  let result = true;
+ let str_expectation_brackets='';
     
  if(str.length%2!=0){
  result = false;
  }  
-    
+ 
  return result;
 }
