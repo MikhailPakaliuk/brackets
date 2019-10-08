92eb321bcad61d74f3fef3ff4c0c54aed6693121
MikhailPakaliuk <mr.dyurilen@mail.ru>
2019-10-08 21:41:22 +0300

    thoughts on how it could be

--- src/index.js
+++ src/index.js
@@ -14,8 +14,15 @@ module.exports = function check(str, bracketsConfig) {
  let str_expectation_brackets='';
     
  if(str.length%2!=0){
- result = false;
- }  
+    result = false;
+ }else{
+    str_expectation_brackets=couple(str[0],bracketsConfig)+str_expectation_brackets;
+    if(str[1]==str_expectation_brackets[0]){
+        str_expectation_brackets=str_expectation_brackets.substring(1);
+    }else{
+    
+    }
+ }
  
  return result;
 }
