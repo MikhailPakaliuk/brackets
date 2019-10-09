6e1573bc0ac34ed29670d98e53ae056347419641
MikhailPakaliuk <mr.dyurilen@mail.ru>
2019-10-09 13:58:30 +0300

    thoughts on how it could be

--- src/index.js
+++ src/index.js
@@ -16,12 +16,24 @@ module.exports = function check(str, bracketsConfig) {
  if(str.length%2!=0){
     result = false;
  }else{
-    str_expectation_brackets=couple(str[0],bracketsConfig)+str_expectation_brackets;
-    if(str[1]==str_expectation_brackets[0]){
-        str_expectation_brackets=str_expectation_brackets.substring(1);//и думаем что делать с str[2]
-    }else{
-        result=(couple(str[1],bracketsConfig)!=undefined);
-    }
+     for(let i=0;str.length>i;i++){
+        let buffer_expectation_brackets=couple(str[i],bracketsConfig);
+        if(buffer_expectation_brackets!=undefined){//still need to check str_expectation_brackets
+            str_expectation_brackets=buffer_expectation_brackets+str_expectation_brackets;
+        }else{
+            result=false;
+        } 
+        if(str[i+1]==str_expectation_brackets[0]){
+            str_expectation_brackets=str_expectation_brackets.substring(1);
+            i+=1;
+        }else{
+            if(couple(str[i+1],bracketsConfig)!=undefined){
+           
+            }else{
+                result=false;
+            }
+        }
+     }
  }
  
  return result;
