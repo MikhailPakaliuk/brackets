3a04130fd5175a223401d88744ee47a80bfed50a
MikhailPakaliuk <mr.dyurilen@mail.ru>
2019-10-09 12:29:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,9 +18,9 @@ module.exports = function check(str, bracketsConfig) {
  }else{
     str_expectation_brackets=couple(str[0],bracketsConfig)+str_expectation_brackets;
     if(str[1]==str_expectation_brackets[0]){
-        str_expectation_brackets=str_expectation_brackets.substring(1);
+        str_expectation_brackets=str_expectation_brackets.substring(1);//и думаем что делать с str[2]
     }else{
-    
+        result=(couple(str[1],bracketsConfig)!=undefined);
     }
  }
  
